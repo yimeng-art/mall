e54111e0c57378645fb64b7417b36245663141fb
yimeng-art <1376343549@qq.com>
2020-12-10 11:51:17 +0800

    '部分完成‘

--- src/router/index.js
+++ src/router/index.js
@@ -1,28 +1,38 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+
+const Home = () => import('../views/home/Home')
+const Category = () => import('../views/category/Category')
+const Profile = () => import('../views/profile/Profile')
+const ShopCart = () => import('../views/shopcart/ShopCart')
 
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
+    path: '',
+    redirect: '/home'
+  },
+  {
+    path: '/home',
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/category',
+    component: Category
+  },
+  {
+    path: '/cart',
+    component: ShopCart
+  },
+  {
+    path: '/profile',
+    component: Profile
   }
 ]
 
 const router = new VueRouter({
   mode: 'history',
-  base: process.env.BASE_URL,
   routes
 })
 
---
+++ src/views/shopcart/ShopCart.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="cart">
+    <nav-bar class="nav-cart">
+<!--      <div slot="center">购物车({{cartLength}})</div>-->
+      <div slot="center">购物车()</div>
+    </nav-bar>
+    <cart-list></cart-list>
+  </div>
+</template>
+
+<script>
+import NavBar from "@/components/common/navbar/NavBar";
+import CartList from "./childComps/CartList";
+import CartBottomBar from "./childComps/CartBottomBar";
+import { mapGetters } from 'vuex'
+export default {
+  name: "ShopCart",
+  components: {
+    NavBar,
+    CartList,
+    CartBottomBar,
+  },
+  computed:{
+    //两种用法：对象或数组
+    // ...mapGetters({
+    //   length: 'cartLength'
+    // }),
+    // ...mapGetters(['cartLength'])
+  }
+}
+</script>
+
+<style scoped>
+.cart{
+  height: 100vh;
+}
+.nav-cart{
+  background-color: var(--color-tint);
+  color: #fff;
+}
+</style>
